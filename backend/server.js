15ccbf15f9fe777d4742ee3a9c3618ec11678e2f
NokDawn <nookdawn@gmail.com>
2020-12-26 16:56:15 +0100

    errorMiddleware on the backend

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/DB.js';
+import { notFound, errorHandler } from './middlewares/errorMiddleware.js';
 
 import productRoutes from './routes/productRoutes.js';
 
@@ -14,6 +15,9 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Listening on Port ${PORT} in ${process.env.NODE_ENV} mode`));
